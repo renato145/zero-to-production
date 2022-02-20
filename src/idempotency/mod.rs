2e96941cd72b76379d202cb98f5d2ab19d7accd9
Luca P <contact@lpalmieri.com>
2022-02-20 17:52:35 +0000

    Insert a row as soon as request processing starts.

--- src/idempotency/mod.rs
+++ src/idempotency/mod.rs
@@ -1,4 +1,4 @@
 mod key;
 mod persistence;
-pub use persistence::{get_saved_response, save_response};
+pub use persistence::{try_processing, NextAction, get_saved_response, save_response};
 pub use key::IdempotencyKey;
--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,7 +1,9 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
+use crate::idempotency::{
+    save_response, try_processing, IdempotencyKey, NextAction,
+};
 use crate::utils::{e400, e500, see_other};
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
@@ -35,11 +37,12 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
-    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+    match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
     {
-        return Ok(saved_response);
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => return Ok(saved_response),
     }
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
